93ac9530f2240edd94b625b815314dfd59349d6a
androidgamedev <36428211+androidgamedev@users.noreply.github.com>
2020-03-21 17:16:18 +0100

    Implemented Map-Components

--- ClientApp/src/components/CustomerMap/CustomerMap.tsx
+++ ClientApp/src/components/CustomerMap/CustomerMap.tsx
@@ -8,26 +8,64 @@ export interface CustomerMapProps {
   className?: string;
 }
 
-export function CustomerMap(props: CustomerMapProps) {
-  const position: LatLngTuple = [52.520008, 13.404954];
-
-  return (
-    <div className={[props.className, "customerMap"].join(" ")}>
-      <Map className='customerMap__map' center={position} zoom={15}>
-        <TileLayer
-          url='https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'
-          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a>'
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup.
-            <br />
-            Easily customizable.
-          </Popup>
-        </Marker>
-      </Map>
-    </div>
-  );
+type State = {
+  hasLocation: boolean,
+  latlng: {
+    lat: number,
+    lng: number,
+  },
 }
 
-export default CustomerMap;
+export default class CustomerMap extends React.Component<{}, State> {
+  mapRef = React.createRef<Map>()
+
+  state = {
+    hasLocation: false,
+    latlng: {
+      lat: 51.505,
+      lng: -0.09,
+    },
+  }
+
+  handleClick = () => {
+    const map = this.mapRef.current
+    if (map != null) {
+      map.leafletElement.locate()
+    }
+  }
+
+  handleLocationFound = (e: Object) => {
+    this.setState({
+      hasLocation: true,
+      latlng: e.latLng
+    })
+  }
+
+  render(){
+    const marker = this.state.hasLocation ? (
+      <Marker position={this.state.latlng}>
+        <Popup>You are here</Popup>
+      </Marker>
+    ) : null
+
+    const position: LatLngTuple = [52.520008, 13.404954];
+
+    return (
+      <div className={["customerMap"].join(" ")}>
+        <Map className='customerMap__map' center={position} zoom={15}>
+          <TileLayer
+            url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup.
+              <br />
+              Easily customizable.
+            </Popup>
+          </Marker>
+        </Map>
+      </div>
+    );
+  }
+}
